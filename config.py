99d2f88a1f1a79a1daf796d0c2b9ade156f61081
hwaseem04 <hajawaseem4@gmail.com>
2023-06-20 19:25:12 +0530

    add other utilities
    - config file to get terminal arguments
    - evaluate file to evalute f1_score and auc
    - predict file to predict labels and probability scores for test data

---
+++ config.py
@@ -0,0 +1,16 @@
+import argparse
+
+class Config:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('--data_path', type=str, default='/ssd_scratch/cvit/hwaseem04/writer/dataset/', help='specify data directory ending with "/"')
+        self.parser.add_argument('--batch_size', type=int, default=8)
+        self.parser.add_argument('--train_size', type=float, default=0.8)
+        self.parser.add_argument('--patch_size', type=int, default=16, help='Patch size for patches')
+        self.parser.add_argument('--epochs', type=int, default=20)
+        self.parser.add_argument('--model_path', type=str, default="/")
+        self.parser.add_argument('--csv_path', type=str, default="/ssd_scratch/cvit/hwaseem04/writer/dataset/val.csv", help="Path to test data csv")
+        self.parser.add_argument('--test_path', type=str, default="/ssd_scratch/cvit/hwaseem04/writer/dataset/val", help="Path to test data")
+    def parse(self):
+        return self.parser.parse_args()
+
---
+++ predict.py
@@ -0,0 +1,290 @@
+import os
+import cv2
+import torch
+import numpy as np
+import pandas as pd
+from glob import glob
+from config import Config
+from scipy import stats as st
+from torch.utils.data import DataLoader
+from model.writerModel import BaseModel
+from load_data import Read_patch, custom_collate
+from sklearn.metrics import f1_score
+
+
+
+# CRAFT
+# ---------------------------------------------------- #
+import time
+import torch
+import torch.backends.cudnn as cudnn
+from craft_model.craft import CRAFT
+from torch.autograd import Variable
+import craft_model.craft_utils
+import craft_model.imgproc
+import craft_model.file_utils
+from collections import OrderedDict
+
+def copyStateDict(state_dict):
+    if list(state_dict.keys())[0].startswith("module"):
+        start_idx = 1
+    else:
+        start_idx = 0
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = ".".join(k.split(".")[start_idx:])
+        new_state_dict[name] = v
+    return new_state_dict
+
+def str2bool(v):
+    return v.lower() in ("yes", "y", "true", "t", "1")
+
+def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net=None):
+
+    # resize
+    img_resized, target_ratio, size_heatmap = craft_model.imgproc.resize_aspect_ratio(image, 1280, interpolation=cv2.INTER_LINEAR, mag_ratio=1.5)
+    ratio_h = ratio_w = 1 / target_ratio
+
+    # preprocessing
+    x = craft_model.imgproc.normalizeMeanVariance(img_resized)
+    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+    if cuda:
+        x = x.cuda()
+
+    # forward pass
+    with torch.no_grad():
+        y, feature = net(x)
+
+    # make score and link map
+    score_text = y[0,:,:,0].cpu().data.numpy()
+    score_link = y[0,:,:,1].cpu().data.numpy()
+
+    # refine link
+    if refine_net is not None:
+        with torch.no_grad():
+            y_refiner = refine_net(y, feature)
+        score_link = y_refiner[0,:,:,0].cpu().data.numpy()
+
+    # Post-processing
+    boxes, polys = craft_model.craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+
+    # coordinate adjustment
+    boxes = craft_model.craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
+    polys = craft_model.craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
+    for k in range(len(polys)):
+        if polys[k] is None: polys[k] = boxes[k]
+
+
+    # render results (optional)
+    render_img = score_text.copy()
+    render_img = np.hstack((render_img, score_link))
+    ret_score_text = craft_model.imgproc.cvt2HeatmapImg(render_img)
+
+    return boxes, polys, ret_score_text
+
+
+# Global definition 
+# Load Craft Model
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+# device = torch.device('mps')
+
+net = CRAFT()     # initialize
+
+print('Loading weights from checkpoint (' + 'craft_model/craft_mlt_25k.pth' + ')')
+net.load_state_dict(copyStateDict(torch.load('craft_model/craft_mlt_25k.pth', map_location=device)))
+
+if torch.cuda.is_available():
+    net = net.cuda()
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = False
+
+net.eval()
+# LinkRefiner
+refine_net = None
+
+main_result = 'result/'
+os.makedirs(main_result, exist_ok=True)
+
+
+def store(image_path, result_folder):
+
+    image = craft_model.imgproc.loadImage(image_path)
+
+    bboxes, polys, score_text = test_net(net, image, 0.7, 0.4, 0.4, torch.cuda.is_available(), False, refine_net)
+
+    # save score text
+    filename, file_ext = os.path.splitext(os.path.basename(image_path))
+    mask_file = result_folder  + filename + '_mask.jpg'
+    cv2.imwrite(mask_file, score_text)
+
+    craft_model.file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
+    # print()
+
+# ----------------------------------------------------- #
+
+def obtain_transform(im, points):
+    x1,y1, x2,y2, x3,y3, x4,y4 = points
+    length = round(abs(x2 - x1))
+    width = round(abs(y3 - y2)) 
+
+    source_vertices = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]], dtype='float32')
+    target_vertices = np.array([[0, 0], [0, length], [width, length], [width, 0]], dtype='float32')
+    M = cv2.getPerspectiveTransform(source_vertices, target_vertices)
+    
+    output_size = (width, length) 
+    blank_image = np.zeros((width, length), dtype=np.uint8)
+    transformed_polygon = cv2.warpPerspective(im, M, output_size).T
+
+    ret, thresh = cv2.threshold(transformed_polygon, 0, 255, cv2.THRESH_OTSU)
+    thresh = 255-thresh
+
+    return thresh
+
+def get_save_patches(img1, img2, label):
+    path1 = img1
+    path2 = img2
+
+    df1 = pd.read_csv(path1.split('.')[0] + '.txt', names=['x1','y1', 'x2','y2', 'x3','y3', 'x4','y4'])
+    df2 = pd.read_csv(path2.split('.')[0] + '.txt', names=['x1','y1', 'x2','y2', 'x3','y3', 'x4','y4'])
+
+    P1 = []
+    P2 = []
+    for i in range(len(df1)):
+        p1_points = df1.iloc[np.random.randint(0,len(df1)), :].values
+        path1 = path1.replace('all_result_test', f'{cfg.data_path}val').replace('res_', '')
+        img1 = cv2.imread(path1, 0)       
+        p1 = obtain_transform(img1, p1_points)
+        P1.append(p1)
+
+    for i in range(len(df2)):
+        p2_points = df2.iloc[np.random.randint(0,len(df2)), :].values
+        path2 = path2.replace('all_result_test', f'{cfg.data_path}val').replace('res_', '')
+        img2 = cv2.imread(path2, 0)
+        p2 = obtain_transform(img2, p2_points)
+        P2.append(p2)
+    
+    if len(P1) == 0 or len(P2) == 0:
+        return 1
+
+    counter = 1
+    for p1 in P1:
+        for p2 in P2:
+            cv2.imwrite(f'result_test/data_1/img_{counter}_{label}.png', p1)
+            cv2.imwrite(f'result_test/data_2/img_{counter}_{label}.png', p2)
+            counter += 1
+    # print(len(P1), len(P2), counter-1)
+    return 0
+    
+
+def predict(model, test_loader):
+    # proba = []
+    pred_label = []
+    for mask, x, y, indices in test_loader:
+        x = x/255
+        mask, x, y = mask.to(device), x.to(device), y.to(device)
+        pred = model(x, mask, indices)
+        D = (pred[:, 0, :] - pred[:, 1, :]).pow(2).sum(1).sqrt()
+        # print(D)
+        # if D >= 1:
+        #     pred_label.append(1)
+        # else:
+        #     pred_label.append(0)
+        pred_label.append(D.cpu().detach().numpy()[0])
+
+        # print(y)
+    print(pred_label)
+    mode = np.mean(np.array(pred_label))
+    print(mode)
+    
+    if mode > 0.2:
+        return 1
+    else:
+        return 0
+    # mode = max(pred_label, key= lambda x: pred_label.count(x))
+    return mode
+
+
+def create_loader():
+    cfg = Config().parse()
+    dataset = Read_patch('result_test', cfg.patch_size)
+    test_loader = DataLoader(dataset, collate_fn=custom_collate, batch_size=cfg.batch_size)
+    return test_loader
+
+if __name__ == '__main__':
+    cfg = Config().parse()
+    csv_path = cfg.csv_path
+    test_path = cfg.test_path
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    # device = torch.device('mps')
+    model = torch.load('model.pth', map_location=device)
+    model.bs = cfg.batch_size
+    df = pd.read_csv(csv_path)
+    prob = []
+    pred_labels = []
+    true_label = []
+    for i in range(len(df)):
+        label = int(df.iloc[i, 2])
+        path1 = os.path.join(test_path, df.iloc[i,0])
+        path2 = os.path.join(test_path, df.iloc[i,1])
+
+        os.system('rm -r result_test')
+        os.makedirs('result_test', exist_ok=True)
+        os.makedirs('result_test/data_1', exist_ok=True)
+        os.makedirs('result_test/data_2', exist_ok=True)
+
+        # store(path1, 'result_test/')
+        # store(path2, 'result_test/')
+
+        # files = glob(os.path.join('result_test', '*.jpg'))
+        # files_final = []
+        # for file in files:
+        #     if 'mask' in file:
+        #         continue
+        #     else:
+        #         files_final.append(file)
+
+        # path1, path2 = files_final # files_final has 2 images only - always
+        path1 = os.path.join('all_result_test', f'res_{df.iloc[i,0]}')
+        path2 = os.path.join('all_result_test', f'res_{df.iloc[i,1]}')
+
+        flag = get_save_patches(path1, path2, label)
+
+        if flag:
+            proba = np.random.rand()
+            # break
+            print('random')
+        else:
+            test_loader = create_loader()
+            pred_label = predict(model, test_loader)
+            proba = np.random.rand()
+        
+        prob.append(proba)
+        pred_labels.append(pred_label)
+        true_label.append(label)
+        f1 = f1_score(y_pred=np.array(pred_labels), y_true= np.array(true_label))
+        print(f'Iteration: {i+1}, Prediction: {pred_label}, Actual: {label}, F1 Score: {f1}')
+        print()
+        print()
+        # break
+    prob = np.array(prob)
+    pred_labels = np.array(pred_labels)
+    # pred_labels =  np.where(prob >= 0.5, 1, 0)
+
+    df['proba'] = prob 
+    df['label'] = pred_labels 
+
+    df.to_csv('Vision_One_2.csv', index=False)
+        
+    
+# # Improvement
+# Proper loss function
+# Proper final layer activation
+# Proper Optimizier - learn
+# Masking tokens in self-attn of encoder (because zero padded)
+# Check whether data pairs are porperly generated. Avoid self similar pairs
+# ? Instead of random patch generation, for each image pair, map all words to each other as similar pair. For dissimilar pair, map atleast 2 from each writer. 
+
+
+# While inference
+# Think about weighted aggregation of prediction
