21bab8463b6960baf139c5ba054be87921373728
hwaseem04 <hajawaseem4@gmail.com>
2023-06-30 22:32:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
         self.parser.add_argument('--batch_size', type=int, default=8)
         self.parser.add_argument('--train_size', type=float, default=0.8)
         self.parser.add_argument('--patch_size', type=int, default=16, help='Patch size for patches')
-        self.parser.add_argument('--epochs', type=int, default=20)
+        self.parser.add_argument('--epochs', type=int, default=5)
         self.parser.add_argument('--model_path', type=str, default="/")
         self.parser.add_argument('--csv_path', type=str, default="/ssd_scratch/cvit/hwaseem04/writer/dataset/val.csv", help="Path to test data csv")
         self.parser.add_argument('--test_path', type=str, default="/ssd_scratch/cvit/hwaseem04/writer/dataset/val", help="Path to test data")
